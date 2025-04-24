bb87783137008a255daac2ebaedd360878efa255
Lucas Brendgen <lucas.brendgen@gmail.com>
2025-04-24 20:56:22 +0200

    feat: adding extraPackages per repository input

--- flake.nix
+++ flake.nix
@@ -2,36 +2,33 @@
   description = "Lukes nixvim configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    nixvim.url = "github:nix-community/nixvim";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    git-hooks-nix.url = "github:cachix/git-hooks.nix";
+    pkgs-by-name-for-flake-parts.url = "github:drupol/pkgs-by-name-for-flake-parts";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    snacks-nvim = {
+      url = "github:folke/snacks.nvim";
+      flake = false;
+    };
+    trouble-nvim = {
+      url = "github:folke/trouble.nvim";
+      flake = false;
+    };
   };
 
-  outputs = {
-    nixvim,
-    flake-parts,
-    ...
-  } @ inputs:
+  outputs = {flake-parts, ...} @ inputs:
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = [
-        "x86_64-linux"
         "aarch64-linux"
-        "x86_64-darwin"
+        "x86_64-linux"
         "aarch64-darwin"
+        "x86_64-darwin"
       ];
-
-      perSystem = {system, ...}: let
-        nixvimLib = nixvim.lib.${system};
-        nixvim' = nixvim.legacyPackages.${system};
-        nixvimModule = {
-          inherit system;
-          module = import ./config;
-          extraSpecialArgs = {};
-        };
-        nvim = nixvim'.makeNixvimWithModule nixvimModule;
-      in {
-        checks.default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
-        packages.default = nvim;
-      };
+      imports = [./flake];
     };
 }
---
+++ flake/devshell.nix
@@ -0,0 +1,11 @@
+{
+  perSystem = {pkgs, ...}: {
+    devShells.default = pkgs.mkShell {
+      name = "Elyth's Nixvim development shell";
+      meta.description = "Shell environment for modifying this Nix configuration";
+      packages = with pkgs; [
+        nixd
+      ];
+    };
+  };
+}
